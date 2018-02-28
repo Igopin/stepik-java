c775b99ac12060569cf98fa44c0af6f98bfc7077
Igor Pinaev <igopin@gmail.com>
2018-02-28 19:11:18 +0300

    updated maven depenedencies; added maven plugins; implemented simple server

---
+++ src/main/java/RequestServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.String;
+
+public class RequestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String value = req.getParameter("key");
+
+        if (value != null) {
+            resp.getWriter().println(value);
+        }
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+}
